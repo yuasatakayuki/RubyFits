f17c9fc9c4956330973a26b6bc01d3474a601e3d
Takayuki Yuasa <yuasatakayuki@gmail.com>
2013-06-10 09:45:54 +0200

    README.mediawiki - API reference URL waa added.

--- documents/createAPIReference.sh
+++ documents/createAPIReference.sh
@@ -2,8 +2,8 @@
 
 # 20130608 Takayuki Yuasa
 
-sub_convertAliasToDef.rb RubyFitsForAPIReference.rb > tmp.rb
-rdoc tmp.rb
+ruby sub_convertAliasToDef.rb RubyFitsForAPIReference.rb > RubyFits.rb
+rdoc RubyFits.rb
 
 cat << EOF
 Open doc/index.html to see the API Reference of RubyFits.
