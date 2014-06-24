461ee9ec81b82ca08c052227565a2dc6342f89ed
Takayuki Yuasa <yuasatakayuki@gmail.com>
2014-06-24 22:09:33 +0900

    example/newImage.rb - image value modified

--- example/newImage.rb
+++ example/newImage.rb
@@ -3,15 +3,27 @@ include Fits
 
 #Create an image HDU whose size is 100x100, and has type of DOUBLE_T 
 
+Width=100
+Height=100
+
 puts Fits::DOUBLE_T
 imageHDU=FitsImageHDU.new
-imageHDU.constructImage(100,100,0,Fits::DOUBLE_T)
+imageHDU.constructImage(Width,Height,0,Fits::DOUBLE_T)
 puts imageHDU.getHDUName
 imageHDU.setHDUName("SampleImage")
 
 puts imageHDU
 puts "Image type = #{imageHDU.getTypeAsString()}"
 puts "Image size = #{imageHDU.getXSize} x #{imageHDU.getYSize}"
+
+puts "Filling values"
+for x in 0...Width
+	for y in 0...Height
+		value=x*y
+		imageHDU.setDouble(value,x,y)
+	end
+end
+
 puts "Setting a value to (50,50)"
 imageHDU.setDouble(3.14159,50,50)
 puts " (50,50) = #{imageHDU.getValue(50,50)}"
